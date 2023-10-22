54d9a12323c1802828c86e911f7ba2817d121019
Philipp Jurasic <philipp.jurasic@tum.de>
2023-10-22 16:29:26 +0200

    editing and login working

--- login.py
+++ login.py
@@ -1,18 +1,43 @@
 import streamlit as st
-def login_page(auth):
+import pyrebase
+import extra_streamlit_components as stx
 
-  email, pw = st.text_input('Email'), st.text_input('Password', type='password')
-  if st.button('Log In'):
-      try:
-          auth.sign_in_with_email_and_password(email, pw)
-      except:
-          st.write('Invalid email or password')
-          
-  if st.button('Sign Up'):
-      try:
-          auth.create_user_with_email_and_password(email, pw)
-      except:
-          st.write('Something went wrong...')
-  st.write('User:', auth.current_user)
+@st.cache_resource()
+def get_manager():
+    return stx.CookieManager()
+
+def login(auth: pyrebase.pyrebase.Auth):
+    if 'auth_user' not in st.session_state:
+        st.session_state['auth_user'] = None
+
+    uid_cookie = get_manager().get('utoken')
+    #st.write('uid_cookie', get_manager().get_all())
+    if uid_cookie:
+        try:
+            auth.sign_in_with_custom_token(uid_cookie)
+            st.write('Logged in as', auth.current_user)
+        except:
+            login_page(auth)
+    else:
+        login_page(auth)
 
 
+def login_page(auth: pyrebase.pyrebase.Auth):
+    email, pw = st.text_input('Email', key="email"), st.text_input('Password', type='password', key="password")
+    if st.button('Log In'):
+        try:
+            auth.sign_in_with_email_and_password(email, pw)
+            #st.write(auth.create_custom_token(auth.current_user["localId"]))
+            #get_manager().set('utoken', auth.create_custom_token(auth.current_user["idToken"]))
+        except:
+            st.write('Invalid email or password')
+            
+    if st.button('Sign Up'):
+        auth_result = None
+        try:
+            auth_result = auth.create_user_with_email_and_password(email, pw)
+            #auth.send_email_verification(auth.current_user["idToken"])
+        except:
+            st.write('Something went wrong...', auth_result)
+    st.write('User:', auth.current_user)
+    st.session_state['auth_user'] = auth.current_user
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,31 +4,51 @@ import login
 import datetime
 
 config = st.secrets
-firebase = pyrebase.initialize_app(config)
-database = firebase.database()
-
-
-
-# Authentication
-auth = firebase.auth()
-login.login_page(auth)
-
-database.remove()
-
-# Create data
-for i in range(3):
-  data = {'userId': auth.current_user, 'entry': "abc", 'date': str(datetime.datetime.utcnow())}
-  database.push(data)
-
-
-
-for entries in database.get().each():
-  with st.chat_message("user"):
-    st.write(entries.val()["entry"])
-
-
-# Title
 st.title('Erfahrungsschatz')
 
-st.chat_input('Start journaling...')
-st.help(st.chat_input)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+if "auth_user" not in st.session_state or not st.session_state["auth_user"]:
+  auth = firebase.auth()
+  login.login(auth)
+
+def entry(data, key):
+  message = data["entry"]
+  date = data["date"]
+  with st.chat_message("human"):
+    col1, col2 = st.columns([8,1])
+    with col1:
+      if "editing" in st.session_state and st.session_state["editing"] == key:
+        edit_inp = st.text_area("Editing entry", value=message, key=key+"_edit")
+        if edit_inp != message:
+          db.child("journals").child(uid).child(key).update({"entry": edit_inp, "date": date})
+          st.write("Updated entry", key)
+          del st.session_state["editing"]
+          st.rerun()
+      else:
+        st.markdown(message)
+    with col2:
+      if st.button("Edit", key=key):
+        st.session_state["editing"] = key
+        st.rerun()
+
+if st.session_state["auth_user"]:
+  uid = st.session_state["auth_user"]["localId"]
+  st.write(st.session_state["auth_user"])
+
+  #.order_by_child("date").limit_to_last(15)
+  entries = db.child("journals").child(uid).get().each()
+  print("fetching all data from db")
+  if entries:
+    for e in entries:
+      entry(e.val(), e.key())
+
+
+  journal_input = st.chat_input('Start journaling...')
+  if journal_input:
+    data = {'entry': journal_input, 'date': str(datetime.datetime.utcnow())}
+    key = db.child("journals").child(uid).push(data)
+    entry(data, key)
+else:
+  st.write("You need to login first")
